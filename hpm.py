bbd0eb96c4a77def259af9585d1ca787eed8921b
JJ Espinoza <4843736+espin086@users.noreply.github.com>
2024-06-12 17:35:03 -0700

    added code generation module

--- hpm.py
+++ hpm.py
@@ -5,12 +5,24 @@ from daily_standup import main as daily_standup_main
 from highlights import main as highlights_main
 from write_email import main as email
 from ticket_generator import main as ticket_generator_main
+from code_generator import main as code_generator_main
 
 page = st.sidebar.title("Navigation")
 page = st.sidebar.radio(
     "Go to",
-    ["Ticket Generator", "Weekly Updates", "Daily Updates", "Emails", "Slack Response"],
+    [
+        "Code Generator",
+        "Ticket Generator",
+        "Weekly Updates",
+        "Daily Updates",
+        "Emails",
+        "Slack Response",
+    ],
 )
+
+
+if page == "Code Generator":
+    code_generator_main()
 
 if page == "Ticket Generator":
     ticket_generator_main()
